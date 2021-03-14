d41776b225f883705946da9e89cd4a3de8964aae
sedasimsek <sdsmsdsm.21@gmail.com>
2021-03-14 18:54:00 +0300

    Updated

---
+++ Core/Utilities/Results/IResult.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    //Temel voidler için bir başlangıç
+    public interface IResult
+    {
+        bool Success { get; } //başarılı mı başarısız mı 
+        string Message { get; } //döndürmek istediğimiz mesaj 
+    }
+}
---
+++ Core/Utilities/Results/Result.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Results
+{
+    public class Result : IResult
+    {
+        //burada this ile dont repeat olmasını engelledik 
+        //yani alttaki Success=success; kod satırını iki kez yazmak yerine böyle yaparak o iki constructorı çalıştırdık.
+        //this:bu classı
+        public Result(bool success, string message):this(success)  
+        {
+            Message = message; 
+        }
+        public Result(bool success) //constructor overloading yapıldı  
+        {
+            //biz ekrana herhangi bir mesaj yazdırmak istemeyebiliriz bu sayede mesaj gondermediği zaman bu constructor çalışacak.
+            Success = success;
+        }
+
+        //IResult 'ın somut sınıfını oluşturduk.
+        public bool Success { get; }
+
+        public string Message { get; }
+    }
+}
